4d88e654afdc452abb043a6bd8590e57f4809f6d
swapnil sutar <swapnilsutar902@gmail.com>
2021-02-16 11:08:29 +0530

    Basic pattern

---
+++ resources/views/answer.blade.php
@@ -0,0 +1,12 @@
+<?php
+   $name = $_POST['1'];
+   $qu = $_POST['111'];
+?>
+
+<h1 class="container">
+   <?php 
+      echo $qu."<br>";
+      echo "Your selected Option ".$name;
+      
+   ?>
+</h1>
